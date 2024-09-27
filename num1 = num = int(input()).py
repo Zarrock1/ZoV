8d37210bb4016bb26b117957a63f81364d52ab32
Zarrock <artemij49@icloud.com>
2024-09-27 16:04:08 +0300

    deleted:    CPP/cpp.exe deleted:    ZoV.code-workspace deleted:    ddosilka.py deleted:    myrep deleted:    num1 = num = int(input()).py deleted:    "\320\232\320\260\320\273\321\214\320\272\321\203\320\273\321\217\321\202\320\276\321\200.py" deleted:    "\320\272\320\260\320\273\321\214\320\272\321\203\320\273\321\217\321\202\320\276\321\2002.py"

--- num1 = num = int(input()).py
+++
@@ -1,7 +0,0 @@
-# This code is taking an input from the user and storing it in the variable `num1`. It is also
-# assigning the same value to the variable `num`.
-num1 = num = int(input())
-num2 = 1
-print(num1)
-print(num1 + num2)
-print(num1 + num2 + 1)
