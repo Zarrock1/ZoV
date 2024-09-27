84074c14ff515d37ab772779fd2dc0f6c7327c65
Zarrock <artemij49@icloud.com>
2024-09-27 16:18:02 +0300

    def calculate_series(n):     sum_series = 0.0     for i in range(1, n + 1):         sum_series += (-1)**(i + 1) / (i * (i + 1))     return sum_series
    # Ввод значения n
    n = int(input("Введите n: "))
    
    # Вычисление и вывод результата
    result = calculate_series(n)
    print(f"Результат: {result}")

--- SiAOD/pr_3/1г.py
+++ SiAOD/pr_3/1г.py
@@ -1,14 +1,12 @@
-def calculate_sum(n):
-    result = 0
-    for k in range(1, n + 1):
-        result += (-1)**(k + 1) / (k * (k + 1))
-    return result
+def calculate_series(n):
+    sum_series = 0.0
+    for i in range(1, n + 1):
+        sum_series += (-1)**(i + 1) / (i * (i + 1))
+    return sum_series
 
 # Ввод значения n
-n = int(input("Введите натуральное число n: "))
+n = int(input("Введите n: "))
 
-# Вычисление суммы
-sum_result = calculate_sum(n)
-
-# Вывод результата
-print(f"Результат суммы: {sum_result}")
+# Вычисление и вывод результата
+result = calculate_series(n)
+print(f"Результат: {result}")
