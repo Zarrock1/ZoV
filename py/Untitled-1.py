e61b7ec976cc0acb46a4e72757a5e418f506f31d
Zarrock <110240071+Zarrock1@users.noreply.github.com>
2024-11-21 11:38:30 +0300

    Delete py directory
    Signed-off-by: Zarrock <110240071+Zarrock1@users.noreply.github.com>

--- py/Untitled-1.py
+++
@@ -1,2 +0,0 @@
-print("\"Khal Drogo's favorite word is \"athjahakar\"\"")
-print("- Did Joffrey agree?\n- He did. He also said \"I love using \\n\".")
--- py/num1 = num = int(input()).py
+++
@@ -1,5 +0,0 @@
-n = int(input("Введите число N: "))
-sum = 0
-for i in range(n):
-    sum += 1 + i / 10
-print("Сумма чисел равна:", sum)
